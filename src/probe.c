4894a710aca50dc5b5a99050c6276359e95d1538
Jia-Lin <Jia-Lin.Shi@rub.de>
2023-05-22 04:04:53 +0200

    draft for spy

--- src/probe.c
+++ src/probe.c
@@ -42,3 +42,15 @@ int probe_precise(char *adrs)
 }
 
 // spy process probing certain memory addresses
+
+/**
+ * loop
+ * probe addresses and safe results in lists (or define length ("buffer") at beginning and just fill array)
+ * wait (how to ensure constant duration of time slots?)
+ * if no cache hits for X times or array full finish and create files with timings
+ */
+
+// default address values (?)
+
+
+
