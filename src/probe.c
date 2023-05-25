23c5c1f7fdde752108a9725e3a108bd2e3632d84
Jia-Lin <Jia-Lin.Shi@rub.de>
2023-05-25 11:40:52 +0200

    debugging_asm kills it

--- src/probe.c
+++ src/probe.c
@@ -1,3 +1,10 @@
+//#include <fcntl.h> // TODO remove unneeded libs
+//#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
 #define THRESHOLD 300
 
 // probe from paper
@@ -5,7 +12,7 @@ int probe_treshold(char *adrs)
 {
     volatile unsigned long time;
 
-    asm __volatile__ (
+    __asm__ __volatile__ (
         " mfence            \n"
         " lfence            \n"
         " rdtsc             \n"
@@ -23,9 +30,10 @@ int probe_treshold(char *adrs)
 
 int probe_precise(char *adrs)
 {
+    printf("entering probe precise\n");
     volatile unsigned long time;
 
-    asm __volatile__ (
+    __asm__ __volatile__ (
         " mfence            \n"
         " lfence            \n"
         " rdtsc             \n"
@@ -52,30 +60,33 @@ int probe_precise(char *adrs)
 void spy(char **target_adrs, int adrs_amount, int probes_amount)
 {
     unsigned long current_probe_time, probe_time;
-    unsigned long adrs_times[adrs_amount][probes_amount]; // persistence TODO later
+    //unsigned long adrs_times[adrs_amount][probes_amount]; // persistence TODO later
     for(int cur_slot=0;cur_slot<probes_amount;cur_slot++)
     {
         current_probe_time = 0;
         for(int cur_adr_i=0;cur_adr_i<adrs_amount;cur_adr_i++)
         {
             char *ptr=target_adrs[cur_adr_i];
+            printf("current adrs %p\n", ptr);
             probe_time=probe_precise(ptr); 
-            printf("measured value for adrs %lu is %lu\n", ptr, probe_time);                             // probe
-            current_current_probe_timetime+=probe_time;                 // sum up probe times
+            printf("measured value for adrs %p is %lu\n", ptr, probe_time);                             // probe
+            current_probe_time+=probe_time;                 // sum up probe times
                                                                         // add timing to array for persistence 
         }
                                                                         // wait 2500 cycles in ns current_probe_time asd (how?)
     }
+    printf("end spy\n");
 }
 
 // default address values (?)
-
-
 int main(int argc, char *argv[])
 {
+    printf("starting\n");
     char *target_adrs[2];
-    target_adrs[0]=0x00000000000967c7;
-    target_adrs[1]=0x0000000000095f5d;
+    target_adrs[0]=(char *)0x00000000000967c7;
+    target_adrs[1]=(char *)0x0000000000095f5d;
     int adrs_amount = 2, pr_amount = 2000;
-    
+    printf("starting spy\n");
+    spy(target_adrs, adrs_amount, pr_amount);
+    printf("ending\n");
 }
