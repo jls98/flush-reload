18ae7174a1567ddc9ac2245aeeb01d4280139556
Jia-Lin <Jia-Lin.Shi@rub.de>
2023-05-25 11:01:50 +0200

    added main for test and completed spy structure

--- src/probe.c
+++ src/probe.c
@@ -49,8 +49,33 @@ int probe_precise(char *adrs)
  * wait (how to ensure constant duration of time slots?)
  * if no cache hits for X times or array full finish and create files with timings
  */
+void spy(char **target_adrs, int adrs_amount, int probes_amount)
+{
+    unsigned long current_probe_time, probe_time;
+    unsigned long adrs_times[adrs_amount][probes_amount]; // persistence TODO later
+    for(int cur_slot=0;cur_slot<probes_amount;cur_slot++)
+    {
+        current_probe_time = 0;
+        for(int cur_adr_i=0;cur_adr_i<adrs_amount;cur_adr_i++)
+        {
+            char *ptr=target_adrs[cur_adr_i];
+            probe_time=probe_precise(ptr); 
+            printf("measured value for adrs %lu is %lu\n", ptr, probe_time);                             // probe
+            current_current_probe_timetime+=probe_time;                 // sum up probe times
+                                                                        // add timing to array for persistence 
+        }
+                                                                        // wait 2500 cycles in ns current_probe_time asd (how?)
+    }
+}
 
 // default address values (?)
 
 
-
+int main(int argc, char *argv[])
+{
+    char *target_adrs[2];
+    target_adrs[0]=0x00000000000967c7;
+    target_adrs[1]=0x0000000000095f5d;
+    int adrs_amount = 2, pr_amount = 2000;
+    
+}
